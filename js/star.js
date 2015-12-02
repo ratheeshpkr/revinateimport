a26c3ea9473445f8563dcdd4b151baaf8b28c578
venkat <venkatesan.tk@sakhatech.com>
2015-12-02 12:05:25 +0530

    Fixed jQuery Conflict

--- js/star.js
+++ js/star.js
@@ -1,15 +1,15 @@
-$(function() {
-    $('span.stars').stars();
+jQuery(function() {
+    jQuery('span.stars').stars();
 });
-$.fn.stars = function() {
-    return $(this).each(function() {
+jQuery.fn.stars = function() {
+    return jQuery(this).each(function() {
         // Get the value
-        var val = parseFloat($(this).html());
+        var val = parseFloat(jQuery(this).html());
         // Make sure that the value is in 0 - 5 range, multiply to get width
         var size = Math.max(0, (Math.min(5, val))) * 16;
         // Create stars holder
-        var $span = $('<span />').width(size);
+        var jQueryspan = jQuery('<span />').width(size);
         // Replace the numerical value with stars
-        $(this).html($span);
+        jQuery(this).html(jQueryspan);
     });
 }
