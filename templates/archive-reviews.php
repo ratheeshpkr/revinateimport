45f45f8f245d0dbc5712b849e17eb8b62501bf14
venkat <venkatesan.tk@sakhatech.com>
2015-12-02 12:06:02 +0530

    Archive Page Template

--- templates/archive-reviews.php
+++ templates/archive-reviews.php
@@ -8,37 +8,49 @@
  * @package     Renivate/Templates
  * @version     0.1
  */
-
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
 get_header( 'snhotel' ); ?>
-
-    
 
         <?php 	while ( have_posts() ) : the_post(); 
 				global $post;
 		?>
 				
             <?php //snhotel_get_template_part( 'content', 'archive-accommodation' ); ?>
-		<div class="container">		
+		<div class="container ratingbox">
+				
+			<div class="title">
+				<?php
+				echo '<strong>Title:</strong>';
+				$meta_avail = get_post_meta($post->ID, 'title', true);?>
+				<a href="<?php the_permalink() ?>"><?php echo $meta_avail; ?></a>
+			</div>
+			
 			<?php
+			echo '</br>'?>
+			<div class="author">
+				<?php
+				echo '<strong>Author:</strong>';
+				$meta_leadTime = get_post_meta($post->ID,'author', true);
+				echo $meta_avail; 
+				?>
+			</div>
 			
-			echo '<strong>Title:</strong>'.$meta_avail = get_post_meta($post->ID, 'title', true); 
-			echo '</br>';
-			echo '<strong>Author:</strong>'.$meta_leadTime = get_post_meta($post->ID,'author', true); 
-			echo '</br>';
-			echo '<strong>Link:</strong>'.$meta_offerPrice = get_post_meta($post->ID,'link', true); 
-			echo '</br>';
-			echo '<strong>Language:</strong>'.$meta_offerPrice = get_post_meta($post->ID,'language', true); 
-			echo '</br>';
-			echo '<strong>Rating:</strong>'.$meta_offerPrice = get_post_meta($post->ID,'rating', true); 
-			echo '</br>';
-			echo '<strong>Subratings:</strong>'.$meta_offerPrice = get_post_meta($post->ID,'subratings', true); 
-			echo '</br>';
-			echo '<strong>Triptype:</strong>'.$meta_offerPrice = get_post_meta($post->ID,'triptype', true); 
-			echo '</br>';
+			<?php
+			echo '</br>'?>
+			<div class="stars">
+				<?php
+				echo '<strong>Rating:</strong>';
+				$meta_offerPrice = get_post_meta(get_the_ID(),'rating', true);
+				echo '</br>'; 
+				echo '<span class="stars">'.$meta_offerPrice.'</span>';
+				echo '</br>';?>
+			</div>
 			
+			<?php
+			echo '<strong>Triptype:</strong>'.$meta_offerPrice = get_post_meta($post->ID,'triptype', true);
+			echo '</br>';
 			?>
+
 		</div>
 
         <?php endwhile; // end of the loop. ?>
