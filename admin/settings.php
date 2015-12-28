86e64162dd37713c3fedbfc547e58239bff5e1d5
prakshi1903 <prakshi.chouhan@sakhatech.com>
2015-12-28 15:48:30 +0530

    cron changes

--- admin/settings.php
+++ admin/settings.php
@@ -3,7 +3,6 @@
 	  wp_enqueue_style('star-css', site_url().'/wp-content/plugins/revinateimport-import/css/star.css');
 	}
 	add_action('admin_head', 'my_custom_fonts');
-	
 	function my_custom_menu_page()
 	{
 		global $wpdb;
@@ -16,7 +15,10 @@
 		do_settings_sections( 'myplugin_options_group' );
 	  ?>
 	  <!--<h3>Please provide below details</h3>-->
-	  
+	  <?php
+	  $var = '';
+	  if(get_option('revinate_email_check') == 1)
+		$var = "checked = 'checked'";?>
 	  <table class="form-table">
 	  <tr valign="top">
 	  <th scope="row">Revinate Hotel ID</th>
@@ -35,11 +37,17 @@
 	  <th scope="row">Revinate API Secret</th>
 	  <td><input type="text" id="revin_settings_secret" name="revin_settings_secret" value="<?php echo get_option('revin_settings_secret'); ?>" class="regular-text "/></td>
 	  </tr>
+	  <tr valign="top">
+	  <th scope="row">Send Mail On Error</th>
+	  <td><input type="checkbox" id="revinate_email_check" name="revinate_email_check" value="1" <?php echo $var;?> /></td>
+	  </tr>
+	  <tr valign="top">
+	  <th scope="row">Enter Email Id</th>
+	  <td><input type="text" id="revinate_email" name="revinate_email" value="<?php echo get_option('revinate_email'); ?>" class="regular-text "/></td>
+	  </tr>
 	  </table>
-	  <?php
+	  <?php	
 	  submit_button();
-	// rev_install_dataa();
-	  
 	  ?>
 	  </form>
 	  </div>
